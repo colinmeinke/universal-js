06ba505f3fa86b96ec4057fde9ef911f07354383
Colin Meinke <hello@colinmeinke.com>
2016-02-14 13:21:10 -0600

    fix(hot): hot reload components

--- src/common/config/routes.js
+++ src/common/config/routes.js
@@ -5,10 +5,17 @@ import Edit from '../components/Edit';
 
 import { updateName } from '../actions/name';
 
-const routes = [
+const getRoutes = () => ([
   [ '/', { name: updateName }, <Home /> ],
   [ 'edit', { name: updateName }, <Edit /> ],
   [ '*', <h1>Not found</h1> ],
-];
+]);
+
+let routes = getRoutes();
+
+if ( __DEVELOPMENT__ && module.hot ) {
+  module.hot.accept();
+  routes = getRoutes();
+}
 
 export default routes;
